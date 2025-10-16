a080969f492de0f6bdca61e449d5221b3766c5fa
Yeremenko-Kyrylo <Yeremenko.kyryll@gmail.com>
2025-10-16 14:37:04 +0300

    added lesson 3

---
+++ Lesson3/src/arifmetics.js
@@ -0,0 +1,15 @@
+const a = 10;
+const b = 3;
+
+console.log('Додавання:', a + b);
+console.log('Віднімання:', a - b);
+console.log('Множення:', a * b);
+console.log('Ділення:', a / b);
+console.log('Залишок від ділення:', a % b);
+
+const str = '5';
+const num = 2;
+
+console.log('Конкатенація:', str + num);
+console.log('Неявне перетворення (додавання):', +str + num);
+console.log('Неявне перетворення (множення):', str * num);
